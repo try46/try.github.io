6920fe8d0dcdb131db92b96425d6070ad545dc7d
try46 <c011610353@edu.teu.ac.jp>
2018-06-13 17:12:04 +0900

    add css

--- tex/tex.js
+++ tex/tex.js
@@ -7,9 +7,9 @@ function myFunc(){
 }
 
 jQuery(document).ready(function(){
-   $('div').mousemove(function(e){
+   $('h1').mousemove(function(e){
      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
-     $('div').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+     $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
    });
 });
