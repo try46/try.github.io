c7e5ad1b1e97c403fa3b5cf97c6bc7e0294f71fb
try46 <c011610353@edu.teu.ac.jp>
2018-06-13 17:16:38 +0900

    add css

--- tex/tex.js
+++ tex/tex.js
@@ -1,11 +1,3 @@
-var myColTbl = new Array("#FF0000","#0000FF","#00FF00","#FF00FF","#00FFFF","#FFFF00","#000000","FF1111");
-var myColCnt = 0;
-
-function myFunc(){
-     document.getElementById("test").style.color = myColTbl[myColCnt];
-     myColCnt = ( myColCnt < myColTbl.length -1 ) ? myColCnt + 1 : 0;
-}
-
 jQuery(document).ready(function(){
    $('h1').mousemove(function(e){
      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
